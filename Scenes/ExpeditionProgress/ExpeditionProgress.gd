146941dfecb5692fe58d100beb36d8fe15c585e7
Hubert Balcerzak <hubertbalc@gmail.com>
2022-09-21 23:31:34 +0200

    expedition ui

---
+++ Scenes/ExpeditionProgress/ExpeditionProgress.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var hedgehog: Sprite
+var startPos:Vector2
+var endPos:Vector2
+export var expeditionTime = 20
+var progress = 0
+
+signal expedition_finished
+
+
+func _ready():
+	hedgehog = get_node("AHedgehog")
+	startPos = get_node("StartPos").position
+	endPos = get_node("EndPos").position
+
+
+func _process(delta):
+	progress += delta/expeditionTime
+	hedgehog.position = startPos.linear_interpolate(endPos, progress)
+	if progress >=1:
+		emit_signal("expedition_finished")
