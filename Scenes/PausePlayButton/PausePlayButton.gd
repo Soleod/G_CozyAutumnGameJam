0fdf514678c9b42d667c6baa75e89858400a9431
Hubert Balcerzak <hubertbalc@gmail.com>
2022-09-22 01:07:27 +0200

    fix pause ui

--- Scenes/PausePlayButton/PausePlayButton.gd
+++ Scenes/PausePlayButton/PausePlayButton.gd
@@ -1,10 +1,18 @@
 extends TextureButton
 
+var pause: Texture
+var unpause: Texture
+
+func _ready():
+	pause = load("res://Art/UI/Pause/Pause.png")
+	unpause = load("res://Art/UI/Pause/Pause_paused.png")
 
 func _pressed():
 	match GameManager.currentGameState:
 		GameManager.GameState.RUNNING:
 			GameManager.ChangeGameState(GameManager.GameState.PAUSED)
+			self.texture_normal = unpause
 		GameManager.GameState.PAUSED:
 			GameManager.ChangeGameState(GameManager.GameState.RUNNING)
+			self.texture_normal = pause
 
