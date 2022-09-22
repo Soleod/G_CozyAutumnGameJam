9e9c66b9552b14ec788b199666c4cbf0092853b8
Hubert Balcerzak <hubertbalc@gmail.com>
2022-09-22 23:29:31 +0200

    spawn other resources

--- Scenes/ItemSpawner.gd
+++ Scenes/ItemSpawner.gd
@@ -2,15 +2,16 @@ extends Area2D
 
 var spawnChancePercentage = 20
 var itemResources: Array
+var fallenItem
 var startArea: Vector2
 var endArea: Vector2
 
 class ItemResource:
-	var resource
+	var texture
 	var type: String
 
-	func _init(resource, type):
-		self.resource = resource
+	func _init(texture, type):
+		self.texture = texture
 		self.type = type
 
 # Called when the node enters the scene tree for the first time.
@@ -18,8 +19,11 @@ func _ready():
 	endArea = $CollisionShape2D.shape.extents
 	startArea = $CollisionShape2D.global_position - endArea
 	GameManager.connect("clock_tick", self, "_on_tick")
+	fallenItem = load("res://Scenes/FallenLeaf/FallenLeaf.tscn")
 	itemResources = [
-		ItemResource.new(load("res://Scenes/FallenLeaf/FallenLeaf.tscn"), "leaves")
+		ItemResource.new(load("res://Art/UI/Resources/Leaves.png"), "leaves"),
+		ItemResource.new(load("res://Art/UI/Resources/Sticks.png"), "sticks"),
+		ItemResource.new(load("res://Art/UI/Resources/Food.png"), "food")
 	]
 
 func _on_tick():
@@ -28,10 +32,11 @@ func _on_tick():
 
 func _spawn_item():
 	var itemToSpawn = itemResources[randi() % itemResources.size()]
-	var spawnedItem = itemToSpawn.resource.instance()
+	var spawnedItem = fallenItem.instance()
 	var spawnPosition = Vector2(rand_range(startArea.x, endArea.x * 2), rand_range(startArea.y, endArea.y))
 	spawnedItem.position = spawnPosition
 	spawnedItem.type = itemToSpawn.type
+	spawnedItem.get_node("Sprite").texture = itemToSpawn.texture
 	add_child(spawnedItem)
 	print("spawned " + itemToSpawn.type + " at " + spawnPosition.x as String + " " + spawnPosition.y as String)
 
