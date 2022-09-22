392d6cb35428c1aab2bf5daf6a1c154d32e4d041
Hubert Balcerzak <hubertbalc@gmail.com>
2022-09-23 00:18:10 +0200

    fix play/pause buttton

--- Scenes/PausePlayButton/PausePlayButton.gd
+++ Scenes/PausePlayButton/PausePlayButton.gd
@@ -6,13 +6,19 @@ var unpause: Texture
 func _ready():
 	pause = load("res://Art/UI/Pause/Pause.png")
 	unpause = load("res://Art/UI/Pause/Pause_paused.png")
+	GameManager.connect("state_change", self, "on_state_change")
 
 func _pressed():
 	match GameManager.currentGameState:
 		GameManager.GameState.RUNNING:
 			GameManager.ChangeGameState(GameManager.GameState.PAUSED)
-			self.texture_normal = unpause
 		GameManager.GameState.PAUSED:
 			GameManager.ChangeGameState(GameManager.GameState.RUNNING)
-			self.texture_normal = pause
 
+
+func on_state_change(current_state):
+	match current_state:
+		GameManager.GameState.RUNNING:
+			self.texture_normal = unpause
+		GameManager.GameState.PAUSED:
+			self.texture_normal = pause
