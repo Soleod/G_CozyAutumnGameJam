10fad22249cf07027eec2d0ddaf6125796a11c1e
Szymon Rodziewicz <szymonrodant@gmail.com>
2022-09-22 14:56:49 +0200

    Strokeless hedgehog

--- Scenes/Hedgehog/Hedgehog.gd
+++ Scenes/Hedgehog/Hedgehog.gd
@@ -17,32 +17,37 @@ func _process(delta):
 	# Calculate the movement distance for this frame
 	var distance_to_walk = speed * delta
 	
-	if path.size() == 0:
-		hedgehogSprite.visible = true
-		animationSprite.visible = false
-		animationPlayer.stop()
+	if GameManager.currentGameState == GameManager.GameState.RUNNING:
+		if path.size() == 0:
+			hedgehogSprite.visible = true
+			animationSprite.visible = false
+			animationPlayer.stop()
+		else:
+			hedgehogSprite.visible = false
+			animationSprite.visible = true
+			if !animationPlayer.is_playing():
+				 animationPlayer.play("Walking")
+		
+		# Move the player along the path until he has run out of movement or the path ends.
+		while distance_to_walk > 0 and path.size() > 0:
+			var distance_to_next_point = position.distance_to(path[0])
+			var hedgehogDirectionValue = position.x - path[0].x
+			if hedgehogDirectionValue < 0:
+				hedgehogSprite.flip_h = true
+				animationSprite.flip_h = true
+			else:
+				hedgehogSprite.flip_h = false
+				animationSprite.flip_h = false
+			if distance_to_walk <= distance_to_next_point:
+				# The player does not have enough movement left to get to the next point.
+				position += position.direction_to(path[0]) * distance_to_walk
+			else:
+				# The player get to the next point
+				position = path[0]
+				path.remove(0)
+			# Update the distance to walk
+			distance_to_walk -= distance_to_next_point
 	else:
-		hedgehogSprite.visible = false
-		animationSprite.visible = true
-		animationPlayer.play("Walking")
-	
-	# Move the player along the path until he has run out of movement or the path ends.
-	if distance_to_walk > 0 and path.size() > 0 and GameManager.currentGameState == GameManager.GameState.RUNNING:
-		var distance_to_next_point = position.distance_to(path[0])
-		var hedgehogDirectionValue = position.x - path[0].x
-		if hedgehogDirectionValue < 0:
-			hedgehogSprite.flip_h = true
-			animationSprite.flip_h = true
-		else:
-			hedgehogSprite.flip_h = false
-			animationSprite.flip_h = false
-		if distance_to_walk <= distance_to_next_point:
-			# The player does not have enough movement left to get to the next point.
-			position += position.direction_to(path[0]) * distance_to_walk
-		else:
-			# The player get to the next point
-			position = path[0]
-			path.remove(0)
-		# Update the distance to walk
-		distance_to_walk -= distance_to_next_point
+		if animationPlayer.is_playing():
+			animationPlayer.stop(false)
 		
